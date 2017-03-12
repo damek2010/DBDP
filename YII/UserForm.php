18b1b3ed85c17d8167267782532c9d388d6619b2
dawid2010 <dszczypinski@interia.pl>
2017-03-12 19:39:03 +0100

    Add files via upload

---
+++ YII/UserForm.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace app\models;
+
+use yii\base\Model;
+
+class UserForm extends Model
+{
+	public $name;
+	public $email;
+	
+	public function rules()
+	{
+		return [
+				[['name','email'],'required'],
+				['email','email'],
+				];
+	}
+	
+	
+	
+}
