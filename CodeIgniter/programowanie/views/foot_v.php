386ed28ed31f88d84e3bbf5d118027ea81da4360
Kyczart <pawopawo@o2.pl>
2017-03-21 23:25:17 +0100

    Add files via upload

---
+++ CodeIgniter/programowanie/views/foot_v.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+</div>  
+				</div>
+				<div class="navbar navbar-default navbar-fixed-bottom">
+					<div class="container">
+						<p class="navbar-text pull-left">© 2017 DBDP SPRINT IS GOOOOD!!!!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
