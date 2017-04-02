a03ea1982acefa2a77c51a40e6638f916dc5bcd3
dawid2010 <dszczypinski@interia.pl>
2017-04-02 12:48:43 +0200

    Add files via upload

--- CodeIgniter/programowanie/models/MenuZ_m.php
+++ CodeIgniter/programowanie/models/MenuZ_m.php
@@ -20,7 +20,7 @@ class MenuZ_m extends CI_Model {
 						  <a href="/ci/projekt_del" class="btn btn-danger btn-lg" role="button"><span class="glyphicon glyphicon-trash"></span> <br/>Usuń</a>
 						</div>
 						<div class="col-xs-6 col-md-6">
-						  <a href="#" class="btn btn-success btn-lg" role="button"><span class="glyphicon glyphicon-knight"></span> <br/>Projekty</a>
+						  <a href="#" class="btn btn-success btn-lg" role="button"><span class="glyphicon glyphicon-list-alt"></span> <br/>Projekty</a>
 						  <a href="#" class="btn btn-warning btn-lg" role="button"><span class="glyphicon glyphicon-repeat"></span> <br/>Sprinty</a>
 						  <a href="/ci/" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-user"></span> <br/>User</a>
 						  <a href="/ci/wyloguj" class="btn btn-danger btn-lg" role="button"><span class="glyphicon glyphicon-off"></span> <br/>Wyloguj</a>
