b6794caa754970595515c6b5683180d5c185732b
dawid2010 <dszczypinski@interia.pl>
2017-04-02 11:16:14 +0200

    Add files via upload

---
+++ CodeIgniter/programowanie/views/zadanie_v.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+
+					<div class="panel-body">
+					    <div class="row">
+						<span class="glyphicon glyphicon-list-alt"></span> Wybierz jedną z opcji z górnego menu</h3>
+					    </div>
+					</div>
+				     
