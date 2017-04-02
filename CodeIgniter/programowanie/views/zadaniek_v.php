90aebfc9c8a40102287c11bc93a97355b7f1caf8
dawid2010 <dszczypinski@interia.pl>
2017-04-02 12:49:00 +0200

    Add files via upload

---
+++ CodeIgniter/programowanie/views/zadaniek_v.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+	<link rel="stylesheet" type="text/css" href="ci/layout/zadanie_css.css" />
+	<div class="panel-body">
+		<div class="row">
+			<div class="col-sm-6 col-md-6">
+				<div class="alert-message alert-message-success">
+					<h3>Aktualności. Dziś jest <strong>02.04.2017</strong><h3>
+					<h4>Zadanie należy do projektu: <strong> Projekt A </strong></h4>
+					<h4>Data realizacji projektu: <strong> 03.01.2017-04.04.2017 </strong></h4>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+							aria-valuemin="0" aria-valuemax="100" style="width:40%">
+							40% Czasu na realizacje projektu
+						</div>
+					</div>
+					<h4>Zadanie należy do kupki sprintu: <strong>Sprint A1</strong></h4>
+					<h4>Data realizacji sprintu: <strong> 03.01.2017-04.04.2017 </strong></h4>
+					<div class="progress">
+						<div class="progress-bar progress-bar-warring" role="progressbar" aria-valuenow="40"
+							aria-valuemin="0" aria-valuemax="100" style="width:80%">
+							80% Czasu realizacji sprintu
+						</div>
+					</div>
+					<h4>Opis Zadania</h4>
+					<p>Opis naszego zadania</p>
+					<h4>Uwagi do zadania</h4>
+					<p>Uwagi do naszego zadania</p>
+				</div>
+			</div>
+		</div>
+	<div>			     
