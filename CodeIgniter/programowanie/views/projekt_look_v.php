7e36f3b4546be3b7d237a53f86821f57f34d223e
Kyczart <pawopawo@o2.pl>
2017-04-15 15:41:38 +0200

    Add files via upload

--- CodeIgniter/programowanie/views/projekt_look_v.php
+++ CodeIgniter/programowanie/views/projekt_look_v.php
@@ -21,7 +21,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				    
 <script type="text/javascript" language="JavaScript 1.5">
 $(document).ready(function() {
-    $('#example').DataTable();
+    $('#example').DataTable({
+        "language": {
+	    "decimal":        "",
+	    "emptyTable":     "Brak dostępnych danych w tabeli",
+	    "info":           "Pokaż _START_ do _END_ z _TOTAL_ wpisów",
+	    "infoEmpty":      "Pokaż 0 do 0 z 0 wpisów",
+	    "infoFiltered":   "(filtered from _MAX_ total wpisów)",
+	    "infoPostFix":    "",
+	    "thousands":      ",",
+	    "lengthMenu":     "Pokaż _MENU_ wpisów",
+	    "loadingRecords": "Ładowanie...",
+	    "processing":     "Przetwarzanie...",
+	    "search":         "Szukaj:",
+	    "zeroRecords":    "Brak pasujących wyników",
+	    "paginate": {
+		"first":      "Pierwszy",
+		"last":       "Ostani",
+		"next":       "Następny",
+		"previous":   "Poprzedni"
+	    },
+	    "aria": {
+		"sortAscending":  ": aktywuj aby sorotwać rosnąco",
+		"sortDescending": ": aktywuj aby sortować malejąco"
+	    }
+	}
+    });
 } );
 </script>
 
--- CodeIgniter/programowanie/views/zadanie_v.php
+++ CodeIgniter/programowanie/views/zadanie_v.php
@@ -6,16 +6,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div class="row">
 			<div class="col-sm-12 col-md-12">
 				<div class="alert-message alert-message-success">
-					<h3>Aktualności. Dziś jest <strong><?php echo date("d-m-Y"); ?></strong><h3>
-					<h4>Zadania należące do projektu: <strong> Projekt A </strong></h4>
+					<h3>Aktualności. Dziś jest <strong><?php echo date("d-m-Y"); ?></strong></h3>
+					<!--<h4>Zadania należące do projektu: <strong> Projekt A </strong></h4>
 					<h4>Data realizacji projektu: <strong> 03.01.2017-04.04.2017 </strong></h4>
 					<div class="progress">
 						<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
 							aria-valuemin="0" aria-valuemax="100" style="width:40%">
 							40% Czasu na realizacje projektu
 						</div>
-					</div>
-					<h3>Lista zadań projektu</h3>
+					</div>-->
+					<?=$lookZC?>
+					<h3>Lista zadań</h3>
 					
 					<p>Wybierz interesujące zadanie</p>
 					<!--<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
