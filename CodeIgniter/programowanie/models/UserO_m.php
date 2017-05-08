e1a7908569b49f6a48252fdcd4f2efd6696c5f6c
lewylewy123 <lewandos14@gmail.com>
2017-05-08 17:12:42 +0200

    Add files via upload

---
+++ CodeIgniter/programowanie/models/UserO_m.php
@@ -0,0 +1,60 @@
+<?php
+class UserO_m extends CI_Model {
+
+	private $wynik;
+	private $src;
+ 
+        public function start($src)
+        {
+			$this->wynik = '';
+			$this->src = $src;
+			$this->wynik .= $this->look();
+			return $this->wynik;
+        }
+	
+	private function look()
+	{
+		$ranga = 'U';
+		if(explode("-:-",$_SESSION['log_zal'])[1] == 'A') {
+			$ranga = 'A';
+		}
+		$zapytanie = 'SELECT * FROM Uzytkownicy;';
+		$query = $this->db->query($zapytanie);
+		$dane = '';
+		$dane .= '<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+				<thead>
+							<tr>
+								<th>ID odpowiedzialności</th>
+								<th>Data przydzielenia</th>
+								<th>ID zadania</th>
+								<th>Procent wykonania</th>
+								<th>Czas trwania</th>
+								<th>Kupka</th>
+								<th>Stan</th>
+							</tr>
+						</thead>
+						<tfoot>
+							<tr>
+								<th>ID odpowiedzialności</th>
+								<th>Data przydzielenia</th>
+								<th>ID zadania</th>
+								<th>Procent wykonania</th>
+								<th>Czas trwania</th>
+								<th>Kupka</th>
+								<th>Stan</th>
+							</tr>
+						</tfoot><tbody>';
+		foreach ($query->result() as $row)
+		{
+			$dane .= '<tr>
+					<td>' . $row->identyfikator . '</td>
+					<td>' . ($ranga=="A"?$row->haslo:str_repeat("*", strlen($row->haslo))) . '</td>
+					<td>' . $row->imie . '</td>
+					<td>' . $row->nazwisko . '</td>
+					<td>' . ($row->ranga=="A"?"Administrator":"Użytkownik") . '</td>
+				</tr>';
+		}
+		$dane .= '</tbody></table>';
+		return $dane;
+	}
+}
