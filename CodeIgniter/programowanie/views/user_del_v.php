4b85e3820b899ff20ce172ed9c12a893914ed791
dawid2010 <dszczypinski@interia.pl>
2017-05-20 13:05:19 +0200

    Add files via upload

---
+++ CodeIgniter/programowanie/views/user_del_v.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+					
+					<div class="panel-body">
+					    <div class="row">
+						<?= $add; ?>
+					    </div>
+					</div>
+
