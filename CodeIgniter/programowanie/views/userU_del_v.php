545eb598d66d50b989d7d9d08d3ed1cf41ca9d97
damek2010 <damian20106@onet.pl>
2017-05-30 21:09:55 +0200

    Add files via upload

--- CodeIgniter/programowanie/views/userU_del_v.php
+++ CodeIgniter/programowanie/views/userU_del_v.php
@@ -2,10 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 
-					
+
 					<div class="panel-body">
 					    <div class="row">
-						<?= $add; ?>
-					    </div>
+						<?= $del; ?>
 					</div>
 
--- CodeIgniter/programowanie/views/zadaniek_v.php
+++ CodeIgniter/programowanie/views/zadaniek_v.php
@@ -2,21 +2,7 @@
 		<div class="row">
 			<div class="col-sm-12 col-md-12">
 				<div class="alert-message alert-message-success">
-					<h3>Aktualności. Dziś jest <strong><?php echo date("d-m-Y") ?></strong></h3><h3>
-					</h3><h4>Zadanie należy do projektu: <strong> Projekt A </strong></h4>
-					<h4>Data realizacji projektu: <strong> 03.01.2017-04.04.2017 </strong></h4>
-					<div class="progress">
-						<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
-							40% Czasu na realizacje projektu
-						</div>
-					</div>
-					<h4>Zadanie należy do kupki sprintu: <strong>Sprint A1</strong></h4>
-					<h4>Data realizacji sprintu: <strong> 03.01.2017-04.04.2017 </strong></h4>
-					<div class="progress">
-						<div class="progress-bar progress-bar-warring" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:80%">
-							80% Czasu realizacji sprintu
-						</div>
-					</div>
+					<h3>Aktualności. Dziś jest <strong><?php echo date("d-m-Y") ?></strong></h3>
 					<?=$zadaniek?>
 				</div>
 			</div>
